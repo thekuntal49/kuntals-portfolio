d21230b275304bb2c26151408b5e5b68575b8644
Kuntal Kargupta <167575898+thekuntal49@users.noreply.github.com>
2025-06-08 22:10:39 +0530

    Added track

--- src/components/global/Visitor.jsx
+++ src/components/global/Visitor.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const Visitor = () => {
   const backendUrl = "https://api.socialclubs.fun";
-  const projectName = "YouTube Channel Tracker";
+  const projectName = "Kuntal's Portfolio";
   const PROJECT_NAME = projectName.toLowerCase().split(" ").join("");
 
   const getBrowserFingerprint = () => {
